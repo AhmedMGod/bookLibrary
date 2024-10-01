c1d22b6f2864c62488afd64914648d71ed79f5e9
AhmedMGod <ahmed_mumin.god@outlook.de>
2024-10-01 12:47:59 +0200

    Add backend and frontend to the project.

---
+++ backendLibrary/src/main/java/org/example/backendlibrary/BackendLibraryApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLibraryApplication.class, args);
+	}
+
+}
---
+++ backendLibrary/src/test/java/org/example/backendlibrary/BackendLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
